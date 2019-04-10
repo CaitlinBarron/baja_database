19ae4fc010cb80162fb4e81b3752f7b819c23934
Unknown <ceb5002@rit.edu>
2019-04-10 02:47:10 -0400

    all add data inputs made

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from tkinter import ttk
+from tkinter import ttk, filedialog
 import gspread
 import datetime
 from oauth2client.service_account import ServiceAccountCredentials
@@ -31,28 +31,27 @@ class Datafile:
         self.collectionDate = datetime.date.today()
         self.car = ""
         self.collectee = ""
-        self.subsystemList = ("Frame", "Suspension", "Steering", "Outboard", "Brakes",
-                     "Ergonomics", "Reduction", "CVT", "Electrical", "R&D",
-                     "Manufacturing", "Engine", "Composites", "Driveline Integration",
-                     "Eboard", "Other")
-        self.subsystem=0
+        self.subsystem = ""
         self.projectName = ""
         self.tags = []
         self.description = ""
+        self.file = ""
 
 
 def gui():
     global root
+    global dataPath
+    dataPath = "C:\\Users\\Caitlin\\Documents\\repositories\\personal code\\baja_database\\testing storage"
     root = Tk()
     root.title("RIT Baja Database")
     root.configure(bg="#FDAC44")
 
     addBtn = Button(root, text="Add Data", width=15, height=2,
-                    bg="#FFFFFF", font=("Arial", "10", "bold"), command=lambda: addData())
+                    bg="#FFFFFF", font=("Arial", "10", "bold"), command=lambda: addDataWindow())
     addBtn.grid(column=0, row=0, padx=20, pady=30)
 
     viewBtn = Button(root, text="View or Edit Data", width=17, height=2,
-                    bg="#FFFFFF", font=("Arial", "10", "bold"), command=lambda: viewData())
+                    bg="#FFFFFF", font=("Arial", "10", "bold"), command=lambda: viewDataWindow())
     viewBtn.grid(column=1, row=0, padx=20, pady=30)
 
     root.mainloop()
@@ -63,58 +62,115 @@ def selDate(dialog, data, dateLbl):
     cal = Calendar(child, data, dateLbl)
 
 
-def addData():
+def fileSel(fileNameLbl):
+    filename = filedialog.askopenfilename(initialdir="/",title="Select file")
+    fileNameLbl.configure(text=filename)
+
+
+def addData(newData, nameEntry, selDateLbl, carEntry, collecteeEntry, subSel, projectEntry, tagsEntry, descEntry, fileNameLbl):
+    newData.dataName = nameEntry.get()
+    newData.car = carEntry.get()
+    newData.collectee = collecteeEntry.get()
+    newData.subsystem = subSel.get()
+    newData.projectName = projectEntry.get()
+    newData.tags = tagsEntry.get().split
+    newData.description = descEntry.get()
+    newData.file = fileNameLbl.cget("text")
+
+
+def addDataWindow():
     newData = Datafile()
     dialog = Toplevel(root)
     dialog.configure(bg="#FDAC44")
 
+    xpad = 2
+    ypad = 2
     namewidth = 15
     nameLbl = Label(dialog, text="Name of Data",
                     bg="#FDAC44", width=namewidth)
-    nameLbl.grid(column=0, row=0, padx=5, pady=2)
+    nameLbl.grid(column=0, row=0, padx=xpad, pady=ypad)
     nameEntry = Entry(dialog, width=namewidth, bg="#FFFFFF")
-    nameEntry.grid(column=0, row=1, padx=5, pady=2)
+    nameEntry.grid(column=0, row=1, padx=xpad, pady=ypad)
 
     datewidth = 13
     dateLbl = Label(dialog, text="Date Collected",
                     bg="#FDAC44", width=datewidth)
-    dateLbl.grid(column=1, row=0, padx=5, pady=2)
+    dateLbl.grid(column=1, row=0, padx=xpad, pady=ypad)
     selDateLbl = Label(dialog, text=newData.collectionDate.strftime("%x"),
                     bg="#FFFFFF", width=datewidth)
-    selDateLbl.grid(column=1, row=1, padx=5, pady=2)
+    selDateLbl.grid(column=1, row=1, padx=xpad, pady=ypad)
     calBtn = Button(dialog, text="Select Date", width=datewidth, bg="#FFFFFF", height=1,
                     command=lambda: selDate(dialog, newData.collectionDate, selDateLbl))
-    calBtn.grid(column=1, row=2, padx=5, pady=2)
+    calBtn.grid(column=1, row=2, padx=xpad, pady=ypad)
 
     carwidth = 10
     carLbl = Label(dialog, text="For Car",
                     bg="#FDAC44", width=carwidth)
-    carLbl.grid(column=2, row=0, padx=5, pady=2)
+    carLbl.grid(column=2, row=0, padx=xpad, pady=ypad)
     carEntry = Entry(dialog, width=carwidth, bg="#FFFFFF")
-    carEntry.grid(column=2, row=1, padx=5, pady=2)
+    carEntry.grid(column=2, row=1, padx=xpad, pady=ypad)
 
     collecteewidth = 20
     collecteeLbl = Label(dialog, text="Who Collected This",
                     bg="#FDAC44", width=collecteewidth)
-    collecteeLbl.grid(column=3, row=0, padx=5, pady=2)
+    collecteeLbl.grid(column=3, row=0, padx=xpad, pady=ypad)
     collecteeEntry = Entry(dialog, width=collecteewidth, bg="#FFFFFF")
-    collecteeEntry.grid(column=3, row=1, padx=5, pady=2)
+    collecteeEntry.grid(column=3, row=1, padx=xpad, pady=ypad)
 
     subwidth=15
     subLbl = Label(dialog, text="Subsystem",
                         bg="#FDAC44", width=collecteewidth)
-    subLbl.grid(column=4, row=0, padx=5, pady=2)
+    subLbl.grid(column=4, row=0, padx=xpad, pady=ypad)
     subSel = ttk.Combobox(dialog, width=collecteewidth,
                         values=["Frame", "Suspension", "Steering", "Outboard", "Brakes",
                         "Ergonomics", "Reduction", "CVT", "Electrical", "R&D",
                         "Manufacturing", "Engine", "Composites", "Driveline Integration",
                         "Eboard", "Other"])
-    subSel.grid(column=4, row=1, padx=5, pady=2)
+    subSel.grid(column=4, row=1, padx=xpad, pady=ypad)
 
-    dialog.mainloop()
+    projwidth=20
+    projectLbl = Label(dialog, text="Project Name",
+                         bg="#FDAC44", width=projwidth)
+    projectLbl.grid(column=5, row=0, padx=xpad, pady=ypad)
+    projectEntry = Entry(dialog, width=projwidth, bg="#FFFFFF")
+    projectEntry.grid(column=5, row=1, padx=xpad, pady=ypad)
+
+    tagswidth = 30
+    tagsLbl = Label(dialog, text="Tags (separate with commas)",
+                       bg="#FDAC44", width=tagswidth)
+    tagsLbl.grid(column=6, row=0, padx=xpad, pady=ypad)
+    tagsEntry = Entry(dialog, width=tagswidth, bg="#FFFFFF")
+    tagsEntry.grid(column=6, row=1, padx=xpad, pady=ypad)
+
+    descwidth = 40
+    descLbl = Label(dialog, text="Data Description",
+                    bg="#FDAC44", width=descwidth)
+    descLbl.grid(column=7, row=0, padx=xpad, pady=ypad)
+    descEntry = Entry(dialog, width=descwidth, bg="#FFFFFF")
+    descEntry.grid(column=7, row=1, padx=xpad, pady=ypad)
+
+    filewidth = 30
+    fileLbl = Label(dialog, text="Data File",
+                    bg="#FDAC44", width=filewidth)
+    fileLbl.grid(column=8, row=0, padx=xpad, pady=ypad)
+    fileNameLbl = Label(dialog, text=newData.file,
+                    bg="#FFFFFF", width=filewidth)
+    fileNameLbl.grid(column=8, row=1, padx=xpad, pady=ypad)
+    fileBtn = Button(dialog, text="Attach File", bg="#FFFFFF", width=int((filewidth/2)),
+                     command=lambda: fileSel(fileNameLbl))
+    fileBtn.grid(column=8, row=2, padx=xpad, pady=ypad)
 
 
-def viewData():
+    submitBtn = Button(dialog, text="Submit Data", width=15,
+                       command=lambda: addData(newData, nameEntry, selDateLbl, carEntry,
+                                               collecteeEntry, subSel, projectEntry,
+                                               tagsEntry, descEntry, fileNameLbl))
+    submitBtn.grid(column=5, row=3, padx=xpad, pady=ypad)
+
+    #dialog.mainloop()
+
+
+def viewDataWindow():
     dialog = Toplevel(root)
 
 
