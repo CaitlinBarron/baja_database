14ed569f8865f8ecad03f60ac3b48f28496d3a81
Unknown <ceb5002@rit.edu>
2019-07-06 03:25:58 -0400

    add data window is better
    still need to change the window titles and add the status bar to add so I can make sure people include required info

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QMainWindow, QDialog, QFileDialog
-from PyQt5.QtCore import QSize
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QMainWindow, QDialog, QFileDialog, QComboBox, QPlainTextEdit, QLineEdit, QDateEdit
+from PyQt5.QtCore import QSize, QDate
 import sys
 import mainUI, addUI, viewUI
 
@@ -35,29 +35,72 @@ class ViewWindow(QDialog, viewUI.Ui_ViewWindow):
 
 
 class AddWindow(QDialog, addUI.Ui_AddWindow):
+    carList = ['',
+                'j-arm',
+                'semi',
+                'r15',
+                'r16',
+                'r17',
+                'r18',
+                'r19',
+                'r20']
+    subsystemList = ['',
+                    'Frame',
+                    'Suspension',
+                    'Steering',
+                    'Outboard',
+                    'Brakes',
+                    'Ergonomics',
+                    'Reduction',
+                    'CVT',
+                    'Electrical',
+                    'R&D',
+                    'Manufacturing',
+                    'Engine',
+                    'Composites',
+                    'Driveline Integration',
+                    'Eboard',
+                    'Other']
+    fileNames = []
+
     def __init__(self, parent=None):
         super(AddWindow, self).__init__(parent)
         self.setupUi(self)
+
         self.submitBtn.clicked.connect(self.submitData)
         self.cancelBtn.clicked.connect(self.cancelButton)
         self.fileBtn.clicked.connect(self.fileBrowse)
+
+        self.carDrop.addItems(self.carList)
+        self.subsystemDrop.addItems(self.subsystemList)
+        self.dateSelect.setDate(QDate.currentDate())
 
 
     def fileBrowse(self):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
-        fileNames, _ = QFileDialog.getOpenFileNames(self,"QFileDialog.getOpenFileNames()", "","All Files (*);;Python Files (*.py)", options=options)
+        files, _ = QFileDialog.getOpenFileNames(self,"QFileDialog.getOpenFileNames()", "","All Files (*);;Python Files (*.py)", options=options)
         filesShort = []
-        if fileNames:
-            print(fileNames)
-            for file in fileNames:
+        if files:
+            print(files)
+            self.fileNames = files
+            for file in files:
                 filesShort.append(file.split('/')[-1])
             uiStr = ', '.join(filesShort)
             self.fileEdit.setText(uiStr)
 
 
     def submitData(self):
-        print('submit button hit')
+        name = self.nameEdit.text()
+        date = self.dateSelect.date()
+        car = self.carDrop.currentText()
+        collectee = self.collecteeEdit.text()
+        subsystem = self.subsystemDrop.currentText()
+        project = self.projectEdit.text()
+        tags = self.tagEdit.selectAll()
+        description = self.descriptionEdit.selectAll()
+        files = self.fileNames
+        print(f"data to submit \nname: {name}\ndate: {date}\ncar: {car}\ncollectee: {collectee}\nsubsytem: {subsystem}\nproject: {project}\ntags: {tags}\ndesc: {description}\nfiles: {files}")
 
 
     def cancelButton(self):
