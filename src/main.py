5104c2fd45c64bc0eba1abb5fade257d5a8def55
Unknown <ceb5002@rit.edu>
2019-09-16 17:50:21 -0400

    finished edit data and add data

--- src/main.py
+++ src/main.py
@@ -9,6 +9,7 @@ import mainUI, detailsUI, editUI, tableUI
 import sqlite3
 import uuid
 import pickle
+import ipdb
 
 '''
 cd C:\\Users\\Caitlin\\Documents\\repositories\\personal code\
@@ -18,6 +19,7 @@ use 'pyuic5 filename.ui -o filename.py' to convert UI files
 '''
 
 CONNECTION = sqlite3.connect('baja_data.db')
+DB_COLS = ["dataID", "name", "date", "car", "collector", "subsystem", "project", "tags", "description", "files"]
 
 
 def strToList(tags):
@@ -90,10 +92,12 @@ class TableWindow(QDialog, tableUI.Ui_TableWindow):
         self.filterBtn.clicked.connect(self.filterData)
         self.populateTable()
 
-    def populateTable(self):
-        cursor = CONNECTION.cursor()
-        cursor.execute('''SELECT * FROM baja_test_table ''')
-        rows = cursor.fetchall()
+    def populateTable(self, rows = []):
+        if not rows:
+            cursor = CONNECTION.cursor()
+            cursor.execute('''SELECT * FROM baja_test_table ''')
+            rows = cursor.fetchall()
+
         self.tableWidget.setColumnCount(10)
         headerList = ['Name', 'Date', 'Car', 'Collector', 'Subsystem', 'Project', 'Tags', 'Description', 'Files', 'id']
         self.tableWidget.setHorizontalHeaderLabels(headerList)
@@ -139,7 +143,9 @@ class TableWindow(QDialog, tableUI.Ui_TableWindow):
 
 
     def filterData(self):
-        print('filter data hit')
+        dialog = EditWindow(filtering = True)
+        dialog.show()
+        dialog.exec_()
 
 
 
@@ -169,7 +175,6 @@ class DetailsWindow(QDialog, detailsUI.Ui_DetailsWindow):
     def deleteData(self):
         msg = QMessageBox()
         msg.setIcon(QMessageBox.Warning)
-
         msg.setText("Are you sure you want to delete this data?")
         msg.setWindowTitle("Delete data?")
         msg.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
@@ -186,14 +191,36 @@ class DetailsWindow(QDialog, detailsUI.Ui_DetailsWindow):
         dialog = EditWindow(self.data)
         dialog.show()
         dialog.exec_()
+        self.updateWindow()
 
 
     def copyData(self):
         print("save a copy of this data")
 
 
+    def updateWindow(self):
+        cursor = CONNECTION.cursor()
+        cursor.execute('SELECT * FROM baja_test_table WHERE dataID=?;', (self.dataId,))
+        newData = cursor.fetchall()[0]
+        for i in range(0,10):
+            self.data[i] = newData[i]
+            if i == 9:
+                self.data[i] = pickle.loads(self.data[i])
+
+        self.nameDisp.setText(self.data[1])
+        self.dateDisp.setText(self.data[2])
+        self.carDisp.setText(self.data[3])
+        self.collectorDisp.setText(self.data[4])
+        self.subsystemDisp.setText(self.data[5])
+        self.projectDisp.setText(self.data[6])
+        self.tagDisp.setPlainText(self.data[7])
+        self.descriptionDisp.setPlainText(self.data[8])
+        self.fileDisp.setText(listToStr(shortFileNames(self.data[9])))
+
+
     def cancelButton(self):
         self.reject()
+
 
 
 class EditWindow(QDialog, editUI.Ui_EditWindow):
@@ -225,19 +252,34 @@ class EditWindow(QDialog, editUI.Ui_EditWindow):
                     'Other']
     fileNames = []
 
-    def __init__(self, data = [],parent=None):
+    def __init__(self, data = [], filtering = False, parent=None):
         super(EditWindow, self).__init__(parent)
         self.setupUi(self)
         self.fileNames = []
         self.data = data
+        self.newData = False
+        if not data and not filtering:
+            self.newData = True
 
-        self.submitBtn.clicked.connect(self.submitData)
         self.cancelBtn.clicked.connect(self.cancelButton)
         self.fileBtn.clicked.connect(self.fileSelect)
-
         self.carDrop.addItems(self.carList)
         self.subsystemDrop.addItems(self.subsystemList)
         self.dateSelect.setDate(QDate.currentDate())
+
+        if filtering:
+            self.submitBtn.setText('Filter')
+            self.requiredLbl.setText('')
+            self.nameLbl.setText('Name')
+            self.dateLbl.setText('Date Collected')
+            self.subsystemLbl.setText('Subsystem')
+            self.descriptionLbl.setText('Description')
+            self.fileLbl.setText('Data File(s)')
+            self.submitBtn.clicked.connect(self.filterData)
+        elif self.newData:
+            self.submitBtn.clicked.connect(self.submitAddData)
+        else:
+            self.submitBtn.clicked.connect(self.submitEditData)
 
         if data:
             self.nameEdit.setText(data[1])
@@ -262,7 +304,7 @@ class EditWindow(QDialog, editUI.Ui_EditWindow):
             self.fileEdit.setText(listToStr(shortFileNames(files)))
 
 
-    def submitData(self):
+    def submitAddData(self):
         name = self.nameEdit.text().lower()
         date = self.dateSelect.date().toString('MM/dd/yyyy')
         car = self.carDrop.currentText().lower()
@@ -283,6 +325,59 @@ class EditWindow(QDialog, editUI.Ui_EditWindow):
             cursor.execute(insertCommand, newData)
             CONNECTION.commit()
             self.cancelButton()
+        else:
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Warning)
+            msg.setWindowTitle("Missing Info!")
+            msg.setText("You are missing required information")
+            confirm = msg.exec_()
+
+
+    def submitEditData(self):
+        updateStrStart = "UPDATE baja_test_table SET"
+        newData = []
+        newData.append(self.data[0])
+        newData.append(self.nameEdit.text().lower())
+        newData.append(self.dateSelect.date().toString('MM/dd/yyyy'))
+        newData.append(self.carDrop.currentText().lower())
+        newData.append(self.collectorEdit.text().lower())
+        newData.append(self.subsystemDrop.currentText())
+        newData.append(self.projectEdit.text().lower())
+        newData.append(self.tagEdit.toPlainText().lower())
+        newData.append(self.descriptionEdit.toPlainText().lower())
+        newData.append(self.fileNames)
+
+        cursor = CONNECTION.cursor()
+
+        if newData[1] and newData[2] and newData[5] and newData[8] and newData[9]:
+            for i in range (1,9):
+                if newData[i] != self.data[i]:
+                    updateStr = f"{updateStrStart} {DB_COLS[i]} = '{newData[i]}' WHERE dataID = '{self.data[0]}';"
+                    cursor.execute(updateStr)
+            if self.fileNames != self.data[9]:
+                updateStr = f"{updateStrStart} {DB_COLS[9]} = ? WHERE dataID = '{self.data[0]}';"
+                cursor.execute(updateStr, (pickle.dumps(newData[9]),))
+            CONNECTION.commit()
+            self.cancelButton()
+        else:
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Warning)
+            msg.setWindowTitle("Missing Info!")
+            msg.setText("You are missing required information")
+            confirm = msg.exec_()
+
+
+    def filterData(self):
+        params = []
+        params.append(self.nameEdit.text().lower())
+        params.append(self.dateSelect.date().toString('MM/dd/yyyy'))
+        params.append(self.carDrop.currentText().lower())
+        params.append(self.collectorEdit.text().lower())
+        params.append(self.subsystemDrop.currentText())
+        params.append(self.projectEdit.text().lower())
+        params.append(self.tagEdit.toPlainText().lower())
+        params.append(self.descriptionEdit.toPlainText().lower())
+        searchStrStart = "SELECT FROM baja_test_table WHERE"
 
 
     def cancelButton(self):
